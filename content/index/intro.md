77d60952673c66a6af72da9b621a13de99c768e8
Trevor Brennan <treb.brennan@gmail.com>
2018-01-05 11:26:54 +1100

    Add home page intro component

---
+++ content/index/intro.md
@@ -0,0 +1,11 @@
+---
+layout: home/intro
+title: About
+imgurl: http://placehold.it/500x400
+imgalt: Intro Alt Tag
+url: /get-started
+---
+
+*Our Design System is based on the [Digital Service Standard](#) and provides a framework and a set of tools to help designers and developers create government products and services faster easier better.*
+
+The system is built on extensive usability research, accessibility conformance to WCAG AA level 2.1 and community engagement. This ensures we can deliver a consistent experience for all users
---
+++ src/layout/home/intro.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+
+/**
+ * The Intro component
+ */
+const Intro = ({ title, url, imgalt, imgurl, _body }) => (
+
+	<div className="intro au-grid">
+		<div className="intro-wrapper row">
+
+			<div className="intro__img col-sm-12 col-md-6 col-md-push-6">
+				<div className="intro__imgpadding">
+					<img className="au-responsive-media-img"
+						src={
+							imgurl.startsWith('http')
+								? imgurl
+								: _relativeURL( imgurl, _ID )
+						}
+					alt={ imgalt } />
+				</div>
+			</div>
+
+			<div className="intro__text col-sm-12 col-md-5 col-md-pull-6">
+				<h2 className="intro__title">{ title }</h2>
+				<div className="content">{ _body }</div>
+				<p className="intro__cta"><a href={ url } className="au-btn au-btn--secondary">Getting Started</a></p>
+			</div>
+
+		</div>
+	</div>
+);
+
+Intro.propTypes = {
+	/**
+	 * tiles:
+	 *   - title: Community
+	 *     imgurl: http://placehold.it/320x320
+	 *     imgalt: Community Alt tag
+	 *     url: /community
+	 *     text: The Design System community has been created for designers and developers to help them transform their agency’s products and services.
+	 */
+	tiles: PropTypes.arrayOf(
+		PropTypes.shape({
+			title: PropTypes.string,
+			imgurl: PropTypes.string,
+			imgalt: PropTypes.string,
+			url: PropTypes.string,
+		})
+	).isRequired,
+};
+
+Intro.defaultProps = {};
+
+export default Intro;
