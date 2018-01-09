b6f4204f9dedba555353aceb13a3944e3d68eb0b
Trevor Brennan <treb.brennan@gmail.com>
2018-01-09 15:47:08 +1100

    Update footer to match visual design

--- src/layout/structure/footer.js
+++ src/layout/structure/footer.js
@@ -8,21 +8,41 @@ import React from 'react';
  */
 const Footer = ({ _body  }) => (
 	<AUfooter className="au-footer au-body au-footer--dark au-body--dark">
+
 		<div className="container-fluid">
-			<div className="row">
-				<div className="col-md-12">
 
-					<AUfooterEnd>
-						<img className="au-responsive-media-img" src="http://placehold.it/157x80" alt="Commonwealth Coat of Arms crest logo" />
+				<div className="row">
+					<div className="col-md-offset-1 col-md-8 col-md-push-3">
 
-						<div className="content">
-							{ _body }
-						</div>
-					</AUfooterEnd>
+						<h2 className="au-display-md">Community</h2>
+						<ul class="au-footer--main-links au-link-list">
+							<li><a href="#">Forums</a></li>
+							<li><a href="#">Articles</a></li>
+							<li><a href="#">Roadmap</a></li>
+							<li><a href="#">Github</a></li>
+						</ul>
+
+						<ul class="au-footer--legal-links au-link-list">
+							<li><a href="#">Privacy policy</a></li>
+							<li><a href="#">Need help? </a></li>
+							<li><a href="#">Accessibility</a></li>
+							<li><a href="#">Security</a></li>
+						</ul>
+
+						<AUfooterEnd>
+							<div className="content au-footer--legal">
+								{ _body }
+							</div>
+						</AUfooterEnd>
+					</div>
+
+					<div className="col-md-3 col-md-pull-9 au-footer-logo">
+						<img className="au-responsive-media-img" src="http://placehold.it/500x200" alt="Commonwealth Coat of Arms crest logo" />
+					</div>
 
 				</div>
 			</div>
-		</div>
+
 	</AUfooter>
 );
 
