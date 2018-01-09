b2dcd7bcc0526431927247ba1af76eafeb974af8
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-01-09 14:14:07 +1100

    fixed example layout
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/layout/code/example.js
+++ src/layout/code/example.js
@@ -17,12 +17,11 @@ const Example = ({ _ID, _relativeURL, pagetitle, modules, example }) => (
 		<title>{ pagetitle }</title>
 
 		<link rel="shortcut icon" type="image/x-icon" href={ _relativeURL( '/assets/img/favicon.ico', _ID ) } />
-		<link rel="stylesheet" href={ _relativeURL( '/assets/css/style.css', _ID ) } />
 		{
 			modules.map( ( module, i ) => {
-				if( Fs.existsSync( Path.normalize(`../../assets/css/${ module }.css`) ) ) {
+				if( Fs.existsSync( Path.normalize(`${ __dirname }/../../assets/css/${ module }.css`) ) ) {
 					return (
-						<link rel="stylesheet" href={ _relativeURL( `/assets/css/${ module }.css`, _ID ) } />
+						<link key={ i } rel="stylesheet" href={ _relativeURL( `/assets/css/${ module }.css`, _ID ) } />
 					);
 				}
 			})
