227bd681fd16cb67d6256827a8b77fe474c5e4d2
Alex Page <alex@alexpage.com.au>
2018-01-17 14:37:40 +1100

    Resetting to the awesome map treb made

--- src/layout/components/component-list-released.js
+++ src/layout/components/component-list-released.js
@@ -10,117 +10,19 @@ const componentListReleased = ({ components, _body }) => (
 		{ _body }
 
 		<ul className="row au-card-list au-card-list--matchheight au-card-list--components">
-			<li className="col-xs-6 col-sm-3">
-				<div className="au-card">
-					<div className="au-card__fullwidth">
-						<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					</div>
-					<h3 className="card__title"><a href="/components/buttons">{ components[0].module }</a></h3>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis asperiores neque hic itaque autem, odit maiores dolores minima similique esse laborum perferendis vero harum beatae. Et natus deleniti rem sequi!</p>
-					<div className="card__version">{ components[0].ver }</div>
-				</div>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<div className="au-card">
-					<div className="au-card__fullwidth">
-						<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					</div>
-					<h3><a href="/components/buttons">{ components[0].module }</a></h3>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-					<div className="ver">{ components[0].ver }</div>
-				</div>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<a href="/components/buttons" className="au-card">
-					<div className="au-card__fullwidth">
-						<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					</div>
-					<h3 className="au-card__link">{ components[0].module }</h3>
-					<div className="ver">{ components[0].ver }</div>
-				</a>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<div className="au-card au-card--shadow">
-					<div className="au-card__fullwidth">
-						<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					</div>
-					<h3><a href="/components/buttons">{ components[0].module }</a></h3>
-					<div className="ver">{ components[0].ver }</div>
-				</div>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<a href="/components/buttons" className="au-card au-card--shadow">
-					<div className="au-card__fullwidth">
-						<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					</div>
-					<h3 className="au-card__link">{ components[0].module }</h3>
-					<div className="ver">{ components[0].ver }</div>
-				</a>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<div className="au-card au-card--shadow au-card--centered">
-					<div className="au-card__fullwidth">
-						<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					</div>
-					<h3><a href="/components/buttons">{ components[0].module }</a></h3>
-					<div className="ver">{ components[0].ver }</div>
-				</div>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<a href="/components/buttons" className="au-card au-card--shadow au-card--centered">
-					<div className="au-card__fullwidth">
-						<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					</div>
-					<h3 className="au-card__link">{ components[0].module }</h3>
-					<div className="ver">{ components[0].ver }</div>
-				</a>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<div className="au-card au-card--shadow">
-					<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					<div className="au-card__bg">
-						<h3><a href="/components/buttons">{ components[0].module }</a></h3>
-						<div className="ver">{ components[0].ver }</div>
-					</div>
-				</div>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<a href="/components/buttons" className="au-card au-card--shadow">
-					<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					<div className="au-card__bg">
-						<h3 className="au-card__link">{ components[0].module }</h3>
-						<div className="ver">{ components[0].ver }</div>
-					</div>
-				</a>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<div className="au-card au-card--detail">
-					<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					<h3><a href="/components/buttons">{ components[0].module }</a></h3>
-					<div className="ver">{ components[0].ver }</div>
-				</div>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<a href="/components/buttons" className="au-card au-card--detail">
-					<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					<h3 className="au-card__link">{ components[0].module }</h3>
-					<div className="ver">{ components[0].ver }</div>
-				</a>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<div className="au-card au-card--detail-left">
-					<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					<h3><a href="/components/buttons">{ components[0].module }</a></h3>
-					<div className="ver">{ components[0].ver }</div>
-				</div>
-			</li>
-			<li className="col-xs-6 col-sm-3">
-				<a href="/components/buttons" className="au-card au-card--detail-left">
-					<img src={ components[0].img } alt="" className="au-responsive-media-img"/>
-					<h3 className="au-card__link">{ components[0].module }</h3>
-					<div className="ver">{ components[0].ver }</div>
-				</a>
-			</li>
+			{
+				components.map( ( component, i ) => (
+					<li key={ i } className="col-xs-6 col-sm-3">
+						<a href="{ components.url }" className="au-card au-card--shadow">
+							<div className="au-card__fullwidth">
+								<img src={ component.img } alt="" className="au-responsive-media-img"/>
+							</div>
+							<h3 className="au-card__link">{ component.module }</h3>
+							<div className="card__version">{ component.version }</div>
+						</a>
+					</li>
+				))
+			}
 		</ul>
 
 	</div>
