611989cfc568618a8c57d3eb6585a3f1d55fdf1d
Trevor Brennan <treb.brennan@gmail.com>
2018-01-22 12:57:32 +1100

    Tidy up furnace (custom build widget)

--- content/download/custom-build.md
+++ content/download/custom-build.md
@@ -1,7 +1,150 @@
 ---
 layout: download/furnace
+components:
+  - name: Core
+    stub: core
+    url: /components/core
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: None
+    required: true
+  - name: Accordion
+    stub: accordion
+    url: /components/accordion
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core, Body
+    required: false
+  - name: Animate
+    stub: animate
+    url: /components/animate
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core, Body
+    required: false
+  - name: Body
+    stub: body
+    url: /components/body
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Breadcrumbs
+    stub: breadcrumbs
+    url: /components/breadcrumbs
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Buttons
+    stub: buttons
+    url: /components/buttons
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Callout
+    stub: callout
+    url: /components/callout
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Control Input
+    stub: control-input
+    url: /components/control-input
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Call to Action Link
+    stub: cta-link
+    url: /components/cta-link
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Direction links
+    stub: direction-links
+    url: /components/direction-links
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Page Footer
+    stub: footer
+    url: /components/footer
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Grid
+    stub: grid-12
+    url: /components/grid-12
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Page header
+    stub: header
+    url: /components/header
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Headings
+    stub: headings
+    url: /components/headings
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Inpage nav
+    stub: inpage-nav
+    url: /components/inpage-nav
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Keyword list
+    stub: keyword-list
+    url: /components/keyword-list
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Link list
+    stub: link-list
+    url: /components/link-list
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Page alerts
+    stub: page-alerts
+    url: /components/page-alerts
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Progress indicator
+    stub: progress-indicator
+    url: /components/progress-indicator
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Responsive media
+    stub: responsive-media
+    url: /components/responsive-media
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Select
+    stub: select
+    url: /components/select
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Skip link
+    stub: skip-link
+    url: /components/skip-link
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Tags
+    stub: tags
+    url: /components/tags
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
+  - name: Text inputs
+    stub: text-inputs
+    url: /components/text-inputs
+    img: http://via.placeholder.com/144x144/ffffff/cccccc
+    dependencies: Core
+    required: false
 ---
 
-## Developers
-
-Choose which components you want to add to your project. To minimise the code bloat, your download will only contain the assets and their dependencies that you add to the build.
+## TEST
--- src/layout/download/furnace.js
+++ src/layout/download/furnace.js
@@ -5,7 +5,7 @@ import React from 'react';
 /**
  * The Intro component
  */
-const DownloadIntro = ({ _body }) => (
+const Furnace = ({ components, _body }) => (
 	<div className="au-grid">
 		<form className="row furnace" method="POST" action="https://furnace.apps.y.cld.gov.au/furnace/">
 
@@ -16,166 +16,111 @@ const DownloadIntro = ({ _body }) => (
 							<a href="#">Clear selections</a>
 						</div>
 
-						<div class="fieldset-wrapper">
+
 							<fieldset>
 								<legend className="sronly">Components</legend>
 
 								<ul className="furnace--component-list">
 
-									<li>
-										<label>
-											<img src="http://via.placeholder.com/144x144/ffffff/cccccc" alt=""/>
+								{
+									components.map( ( components, i ) => (
+										<li key={ i }>
+							        <label>
+							          <span className="control">
+							            <input type="checkbox" name="components" className="au-control-input__input" value={ components.stub }
+														required={ components.required }
+														checked={ components.required }
+														disabled={ components.required }
+													/>
+							            <span className="au-control-input__text"><span className="sronly">Add</span> { components.name } {
+														components.required
+															? '(required)'
+															: ''
+													}</span></span>
+							        </label>
+							        <div className="details">
+							          <img src={ components.img } alt=""/>
+							          <p className="notes">Dependencies: { components.dependencies } </p>
+							          <p className="docs"><a href={ components.url }><span className="sronly">{ components.name }</span> Documentation</a></p>
+							        </div>
+							      </li>
+									))
+								}
 
-											<div className="furnace--component-detail">
-												<h4>Accordion</h4>
-												<p>Dependencies: Core, Body <br/>
-													<a href="/components/buttons/"><span class="sronly">Accordion</span> Documentation</a>
-												</p>
-											</div>
-
-											<div className="furnace--component-actions">
-												<input type="checkbox" name="components" value="accordion" />Add <span class="sronly">Accordion</span><br/>
-											</div>
-										</label>
-									</li>
-
-
-									<li>
-										<label>
-											<img src="http://via.placeholder.com/144x144/ffffff/cccccc" alt=""/>
-
-											<div className="furnace--component-detail">
-												<h4>Animate</h4>
-												<p>Dependencies: Core, Body <br/>
-													<a href="/components/buttons/"><span class="sronly">Animate</span> Documentation</a>
-												</p>
-											</div>
-
-											<div className="furnace--component-actions">
-												<input type="checkbox" name="components" value="animate" />Add <span class="sronly">animate</span><br/>
-											</div>
-										</label>
-									</li>
-
-
-									<li>
-										<label><input type="checkbox" name="components" value="body" />Body</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="breadcrumbs" />Breadcrumbs</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="buttons" />Buttons</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="callout" />Callout</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="control-input" />Control input</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="core" checked disabled />Core</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="cta-link" />Call to action link</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="direction-links" />Direction links</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="footer" />Footer</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="grid-12" />12 Column grid</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="header" />Header</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="headings" />Headings</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="inpage-nav" />In page nav</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="keyword-list" />Keyword list</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="link-list" />Link list</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="page-alerts" />Page alerts</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="progress-indicator" />Progress indicator</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="responsive-media" />Responsive media</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="select" />Select</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="skip-link" />Skip link</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="tags" />Tags</label>
-									</li>
-									<li>
-										<label><input type="checkbox" name="components" value="text-inputs" />Text inputs</label>
-									</li>
 								</ul>
 
 							</fieldset>
-						</div>
+
 			</div>
 
 			<div className="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
 				<h3 className="furnace--border">Your build</h3>
 				<h4>NPM</h4>
-				<div>
-					Your code
-					<a href="#" className="au-btn">Copy</a>
+				<div className="furnace--buildbox">
+					<div className="furnace--code">
+						npm i <br/>
+						@gov.au/body<br/>
+						@gov.au/breadcrumbs <br/>
+						@gov.au/animate <br/>
+						--save
+					</div>
+					<a href="#" className="au-btn au-btn--size-sm">Copy</a>
 				</div>
 
 				<h4>Download Zip</h4>
-				<div class="fieldset">
+
+				<div className="furnace--buildbox">
 					<fieldset>
 						<legend>Stylesheets</legend>
-						<label>
-							<input type="radio" name="styleOutput" value="css" checked/>CSS minified
-						</label><br/>
-						<label>
-							<input type="radio" name="styleOutput" value="cssModules" />CSS modules
-						</label><br/>
-						<label>
-							<input type="radio" name="styleOutput" value="sassModules" />SASS modules
+						<label className="au-control-input au-control-input--block">
+							<input type="radio" name="styleOutput" value="css" className="au-control-input__input" checked/>
+							<span className="au-control-input__text">CSS minified</span>
 						</label>
+
+						<label className="au-control-input au-control-input--block">
+							<input type="radio" name="styleOutput" value="cssModules" className="au-control-input__input" />
+							<span className="au-control-input__text">CSS modules</span>
+						</label>
+
+						<label className="au-control-input au-control-input--block">
+							<input type="radio" name="styleOutput" value="sassModules"  className="au-control-input__input" />
+							<span className="au-control-input__text">SASS modules</span>
+						</label>
+
 					</fieldset>
+
 					<fieldset>
 						<legend>JavaScript</legend>
-						<label>
-							<input type="radio" name="jsOutput" value="js" checked/>JavaScript minified
-						</label><br/>
-						<label>
-							<input type="radio" name="jsOutput" value="jsModules" />JavaScript modules
-						</label><br/>
-						<label>
-							<input type="radio" name="jsOutput" value="react" />React modules
+
+						<label className="au-control-input au-control-input--block">
+							<input type="radio" name="jsOutput" value="js" checked className="au-control-input__input"/>
+							<span className="au-control-input__text">JavaScript minified</span>
 						</label>
+
+						<label className="au-control-input au-control-input--block">
+							<input type="radio" name="jsOutput" value="jsModules" className="au-control-input__input"/>
+							<span className="au-control-input__text">JavaScript modules</span>
+						</label>
+
+						<label className="au-control-input au-control-input--block">
+							<input type="radio" name="jsOutput" value="react" className="au-control-input__input"/>
+							<span className="au-control-input__text">React modules</span>
+						</label>
+
 					</fieldset>
 				</div>
 				<button type="submit" className="au-btn au-btn--block">Download</button>
+
 			</div>
 
 		</form>
 	</div>
 );
 
-DownloadIntro.propTypes = {
+Furnace.propTypes = {
 
 };
 
-DownloadIntro.defaultProps = {};
+Furnace.defaultProps = {};
 
-export default DownloadIntro;
+export default Furnace;
