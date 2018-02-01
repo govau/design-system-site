b81d3371e420eb6c09002e40e892682bbc352907
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-02-01 13:43:32 +1100

    added title to released components
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/layout/component/released.js
+++ src/layout/component/released.js
@@ -20,6 +20,7 @@ const ComponentReleased = ({ cardList, _ID, _body, _parseYaml, _relativeURL }) =
 			link: _relativeURL( `/components/${ component.ID }`, _ID ),
 			rows: [{
 				type: 'svg',
+				title: component.name,
 				svg: _relativeURL( `/assets/svg/map.svg#${ component.ID }`, _ID ),
 				description: component.description,
 				fullwidth: true,
