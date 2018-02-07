c43d3eca90dee57ab4f6cf94ec94d96572eafe46
Alex Page <alex@alexpage.com.au>
2018-02-07 16:08:34 +1100

    Wrapper element and child element same class fail

--- src/js/a11y/030-filter.js
+++ src/js/a11y/030-filter.js
@@ -1,25 +1,25 @@
 var filters = document.querySelector( '.js-filter' );
 if( filters !== null ) {
-	var active = document.getElementById('filter-stylesheet').getAttribute('href').split('/');
-	active = active[ active.length - 1 ].replace('.css', '');
+	var active = document.getElementById( 'filter-stylesheet' ).getAttribute( 'href' ).split( '/' );
+	active = active[ active.length - 1 ].replace( '.css', '' );
 
 	filters.insertAdjacentHTML(
 		'beforebegin',
 		'<div class="bar">' +
-		'<button type="button" class="a11y-button js-filter' + ( active == 'deuteranopia' ? ' is-active' : '' ) + '" data-filter="deuteranopia">Deuteranopia</button>' +
-		'<button type="button" class="a11y-button js-filter' + ( active == 'tritanopia' ? ' is-active' : '' ) + '" data-filter="tritanopia">Tritanopia</button>' +
+		'<button type="button" class="a11y-button js-filter-btn' + ( active == 'deuteranopia' ? ' is-active' : '' ) + '" data-filter="deuteranopia">Deuteranopia</button>' +
+		'<button type="button" class="a11y-button js-filter-btn' + ( active == 'tritanopia' ? ' is-active' : '' ) + '" data-filter="tritanopia">Tritanopia</button>' +
 		'</div>'
 	);
 }
 
-AddEvent( document.querySelectorAll( '.js-filter' ), 'click', function( event, $this ) {
-	var sheet = document.getElementById('filter-stylesheet');
-	var filter = $this.getAttribute('data-filter');
+AddEvent( document.querySelectorAll( '.js-filter-btn' ), 'click', function( event, $this ) {
+	var sheet = document.getElementById( 'filter-stylesheet' );
+	var filter = $this.getAttribute( 'data-filter' );
 	var url = sheet.getAttribute( 'data-' + filter );
 
-	sheet.setAttribute('href', url);
+	sheet.setAttribute( 'href', url );
 
-	var allButtons = document.querySelectorAll( '.js-filter' );
+	var allButtons = document.querySelectorAll( '.js-filter-btn' );
 	for( var i = 0; i < allButtons.length; i++ ) {
 		RemoveClass( allButtons[ i ], 'is-active' );
 	}
