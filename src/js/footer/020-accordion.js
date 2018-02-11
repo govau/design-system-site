d67cc56192c20045200a4e2420a2c2386b0d62b5
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-02-11 11:52:08 +1100

    moved code into file so we can find it
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/js/footer/020-accordion.js
+++ src/js/footer/020-accordion.js
@@ -4,11 +4,3 @@ AddEvent( accordions, 'click', function( event, $this ) {
 	PreventEvent( event );
 	AU.accordion.Toggle( $this );
 });
-
-
-var fastAccordions = document.querySelectorAll( '.js-header-accordion' );
-
-AddEvent( fastAccordions, 'click', function( event, $this ) {
-	PreventEvent( event );
-	AU.accordion.Toggle( $this, 150 );
-});
