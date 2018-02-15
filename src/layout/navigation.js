d5b74dd177262580f9424d7cf1eb16231bd7a11e
Alex Page <alex@alexpage.com.au>
2018-02-16 09:12:20 +1100

    Add table page

--- src/layout/navigation.js
+++ src/layout/navigation.js
@@ -25,7 +25,6 @@ export const CreateLink = ( link, _relativeURL, _ID, _pages ) => {
 			className: `${ active ? 'mainmenu--active' : ''  }${ _isActiveTrail ? ' mainmenu--active-trail' : ''  }`,
 		}
 	}
-
 };
 
 
