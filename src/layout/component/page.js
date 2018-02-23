9d0767c2b6d3fae2ad3adeb93ec8d390e8a38cd2
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-02-23 11:10:09 +1100

    fixed #12
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/layout/component/page.js
+++ src/layout/component/page.js
@@ -33,9 +33,31 @@ const ComponentPage = ({
 <meta name="viewport" content="width=device-width">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+<link rel="apple-touch-icon" sizes="180x180" href=${ _relativeURL('/assets/favicons/apple-touch-icon.png', _ID ) }>
+<link rel="icon" type="image/png" sizes="32x32" href=${ _relativeURL('/assets/favicons/favicon-32x32.png', _ID ) }>
+<link rel="icon" type="image/png" sizes="16x16" href=${ _relativeURL('/assets/favicons/favicon-16x16.png', _ID ) }>
+<link rel="manifest" href=${ _relativeURL('/assets/favicons/site.webmanifest', _ID ) }>
+<link rel="mask-icon" href=${ _relativeURL('/assets/favicons/safari-pinned-tab.svg', _ID ) } color="#06262d">
+<link rel="shortcut icon" href=${ _relativeURL('/assets/favicons/favicon.ico', _ID ) }>
+<meta name="msapplication-config" content=${ _relativeURL('/assets/favicons/browserconfig.xml', _ID ) }>
+<meta name="msapplication-TileColor" content="#000000">
+<meta name="theme-color" content="#ffffff">
+<meta name="robots" content="index, follow">
+<meta name="author" content="GOLD team">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="GOLD">
+<meta name="twitter:description" content="Government open language for design design system">
+<meta name="twitter:name" content="GOLD">
+<meta name="twitter:image" content="https://gold.service.gov.au/assets/favicons/img/gold.png">
+<meta property="og:type" content="website">
+<meta property="og:title" content="GOLD">
+<meta property="og:site_name" content="GOLD">
+<meta property="og:description" content="Government open language for design design system">
+<meta property="og:image" content="https://gold.service.gov.au/assets/favicons/img/gold.png">
+<meta property="og:url" content="https://gold.service.gov.au">
+
 <title>${ pagetitle } - Australian Government Open Language for Design</title>
 
-<link rel="shortcut icon" type="image/x-icon" href=${ _relativeURL( '/assets/img/favicon.ico', _ID ) }>
 <link rel="stylesheet" href=${ _relativeURL( '/assets/css/style.css', _ID ) }>
 
 <!--[if lte IE 9]>
--- src/layout/example/page.js
+++ src/layout/example/page.js
@@ -10,22 +10,45 @@ import Fs from 'fs';
  * @disable-docs
  */
 const Example = ({ _ID, _relativeURL, pagetitle, example, alignment, fullwidth }) => {
+
 	const headContent = `
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 
-	<title>${ pagetitle } - Australian Government Open Language for Design</title>
+<link rel="apple-touch-icon" sizes="180x180" href=${ _relativeURL('/assets/favicons/apple-touch-icon.png', _ID ) }>
+<link rel="icon" type="image/png" sizes="32x32" href=${ _relativeURL('/assets/favicons/favicon-32x32.png', _ID ) }>
+<link rel="icon" type="image/png" sizes="16x16" href=${ _relativeURL('/assets/favicons/favicon-16x16.png', _ID ) }>
+<link rel="manifest" href=${ _relativeURL('/assets/favicons/site.webmanifest', _ID ) }>
+<link rel="mask-icon" href=${ _relativeURL('/assets/favicons/safari-pinned-tab.svg', _ID ) } color="#06262d">
+<link rel="shortcut icon" href=${ _relativeURL('/assets/favicons/favicon.ico', _ID ) }>
+<meta name="msapplication-config" content=${ _relativeURL('/assets/favicons/browserconfig.xml', _ID ) }>
+<meta name="msapplication-TileColor" content="#000000">
+<meta name="theme-color" content="#ffffff">
+<meta name="robots" content="index, follow">
+<meta name="author" content="GOLD team">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="GOLD">
+<meta name="twitter:description" content="Government open language for design design system">
+<meta name="twitter:name" content="GOLD">
+<meta name="twitter:image" content="https://gold.service.gov.au/assets/favicons/img/gold.png">
+<meta property="og:type" content="website">
+<meta property="og:title" content="GOLD">
+<meta property="og:site_name" content="GOLD">
+<meta property="og:description" content="Government open language for design design system">
+<meta property="og:image" content="https://gold.service.gov.au/assets/favicons/img/gold.png">
+<meta property="og:url" content="https://gold.service.gov.au">
 
-	<link rel="shortcut icon" type="image/x-icon" href=${ _relativeURL( '/assets/img/favicon.ico', _ID ) }>
-	<link rel="stylesheet" href=${ _relativeURL( '/assets/css/example.css', _ID ) } />
+<title>${ pagetitle } - Australian Government Open Language for Design</title>
 
-	<!--[if lte IE 9]>
-		<script src="${ _relativeURL( '/assets/js/html5shiv.js', _ID ) }"></script>
-		<script src="${ _relativeURL( '/assets/js/respond.js', _ID ) }"></script>
-	<![endif]-->
+<link rel="stylesheet" href=${ _relativeURL( '/assets/css/example.css', _ID ) } />
 
-	<script src=${ _relativeURL( '/assets/js/header.js', _ID ) }></script>`;
+<!--[if lte IE 9]>
+	<script src="${ _relativeURL( '/assets/js/html5shiv.js', _ID ) }"></script>
+	<script src="${ _relativeURL( '/assets/js/respond.js', _ID ) }"></script>
+<![endif]-->
+
+<script src=${ _relativeURL( '/assets/js/header.js', _ID ) }></script>`;
 
 	return (
 		<html>
