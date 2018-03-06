336750337b8303372ac176a638c7a7d094da6e48
Trevor Brennan <treb.brennan@gmail.com>
2018-03-06 15:36:02 +1100

    Add statisitics to home page

---
+++ content/index/statisitics.md
@@ -0,0 +1,16 @@
+---
+layout: home/statistics
+title: Usage statistics
+btnURL: /components
+btntext: See our Performance Dashboard
+
+tiles:
+  - description: Live projects
+    number: XX
+
+  - description: Github stars
+    number: 145
+
+  - description: Downloads
+    number: XX
+---
---
+++ src/layout/home/statistics.js
@@ -0,0 +1,74 @@
+import AUheading from '../../_uikit/layout/headings';
+import AUbutton from '../../_uikit/layout/buttons';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+
+/**
+ * The Promo component
+ */
+const Features = ({ title, btnURL, btntext, tiles, _relativeURL, _ID, _parseMD, _body }) => (
+	<div className="features statistics au-grid">
+		<div className="row">
+			<div className="features__intro col-md-6 col-md-offset-3">
+				<h2 className="features__intro__headline">{ title }</h2>
+			</div>
+		</div>
+
+		<ul className="features__list row">
+			{
+				tiles.map( ( tile, i ) => (
+					<li className="col-sm-4" key={ i }>
+						<div className="feature__wrapper">
+							<span className="feature__wrapper__headline au-display-xxxl">{ tile.number }</span>
+							<span> { tile.description }</span>
+						</div>
+					</li>
+				))
+			}
+		</ul>
+		{
+		// <div className="features__cta">
+		// 	<a href={ btnURL }>{ btntext }</a>
+		// </div>
+		}
+	</div>
+);
+
+
+Features.propTypes = {
+	/**
+	 * title: What makes our components great
+	 */
+	title: PropTypes.string.isRequired,
+
+	/**
+	 * btnURL: /components
+	 */
+	btnURL: PropTypes.string.isRequired,
+
+	/**
+	 * btntext: See an examples
+	 */
+	btntext: PropTypes.string.isRequired,
+
+	/**
+	* tiles:
+	* - description: Live projects
+	*   number: XX
+	* - description: Github stars
+	*   number: XX
+	* - description: Downloads
+	*   number: XX
+	 */
+	tiles: PropTypes.arrayOf(
+		PropTypes.shape({
+			smltitle: PropTypes.string,
+			text: PropTypes.string,
+		})
+	).isRequired,
+};
+
+Features.defaultProps = {};
+
+export default Features;
