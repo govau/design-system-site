477bdfbbdf0ad0fc50c814a140b0d84d05726de9
Alex Page <alex@alexpage.com.au>
2018-09-24 16:53:14 +1000

    Start writing sort

--- src/layout/side-nav.js
+++ src/layout/side-nav.js
@@ -3,7 +3,30 @@ import AUsideNav           from '../_uikit/layout/side-nav';
 import React, { Fragment } from 'react';
 import PropTypes           from 'prop-types';
 
-const SideNav = ({ _relativeURL, _ID, _nav, _pages }) => {
+const SideNav = ({ order, _relativeURL, _ID, _nav, _pages }) => {
+
+	const SortNavigation = ( a, b ) => {
+		const indexOfA = order.indexOf( a.link );
+		const indexOfB = order.indexOf( b.link );
+
+		if ( indexOfA === -1 ){
+			indexOfA = order.length + 1;
+		}
+
+		if ( indexOfB === -1 ){
+			indexOfB = order.length + 1;
+		}
+
+		if ( indexOfA < indexOfB ){
+			return -1;
+		}
+
+		if ( indexOfA > indexOfB ){
+			return 1;
+		}
+
+		return 0;
+	}
 
 	const CreateNavigation = ( nav ) => Object.entries( nav )
 		.map( ([ key, value ]) => {
@@ -21,14 +44,20 @@ const SideNav = ({ _relativeURL, _ID, _nav, _pages }) => {
 			return link;
 		});
 
-	const getStartedNav = CreateNavigation( _nav.index[ 'get-started' ] );
+	let navItems = CreateNavigation( _nav.index[ 'get-started' ] );
+
+	// If there is an order, sort the navigation
+	if( order ){
+		navItems.sort( SortNavigation );
+	}
+
 
 	return(
 		<AUsideNav
 			accordionHeader="More information on getting started"
 			menuHeaderLink="/get-started"
 			menuHeader="Get started"
-			items={ getStartedNav }
+			items={ navItems }
 		/>
 	);
 };
