797e24652f187cd036543033d55a2a20946445e3
Alex Page <alex@alexpage.com.au>
2018-09-24 10:31:39 +1000

    Renaming basic page moving side nav

--- src/layout/side-nav.js
+++ src/layout/side-nav.js
@@ -1,4 +1,4 @@
-import AUsideNav           from '../../_uikit/layout/side-nav';
+import AUsideNav           from '../_uikit/layout/side-nav';
 
 import React, { Fragment } from 'react';
 import PropTypes           from 'prop-types';
