9f57b1dab6ee546736b99b484d96cc8bfbfa6e6a
Adam A. Zerella <adamzerella@users.noreply.github.com>
2018-10-30 15:25:12 +1100

    Bracket spacing improvement

--- src/helper/mkdir.js
+++ src/helper/mkdir.js
@@ -7,9 +7,9 @@ module.exports = {
      * @see https://nodejs.org/api/fs.html#fs_fs_mkdir_path_options_callback
      */
     CreateDirectory: function( path ) {
-        if (!Fs.existsSync(path)) {
+        if ( !Fs.existsSync( path ) ) {
             Fs.mkdirSync( path, { recursive: true }, ( error ) => {
-                if (error) throw error;
+                if ( error ) throw error;
               });
         }
         console.log(`Directory ${path} already exists, no need to create...`)
@@ -17,4 +17,4 @@ module.exports = {
 }
 
 // Given the first argument e.g "node this-script.js /path/to/create"
-module.exports.CreateDirectory(process.argv[2]);
+module.exports.CreateDirectory( process.argv[2] );
