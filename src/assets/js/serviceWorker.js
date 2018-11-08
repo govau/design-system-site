b72988511d326ee0870a77ec84bb1eef0f3242bd
adamzerella <adamzerella@users.noreply.github.com>
2018-11-08 13:25:11 +1100

    Added more meta information to site.webmanifest
    Added more cache paths for serviceWorker

--- src/assets/js/serviceWorker.js
+++ src/assets/js/serviceWorker.js
@@ -7,15 +7,23 @@ const CACHE = [
 	"/assets/css/style.css",
 	"/assets/js/header.js",
 	"/assets/js/footer.js",
-	"/assets/img/users-logo-dta.png",
+	"/assets/favicons/site.webmanifest",
+	"/assets/img/artwork-about-community.png",
+	"/assets/img/artwork-about-components.png",
+	"/assets/img/artwork-about-templates.png",
 	"/assets/img/artwork-about.png",
-	"/assets/img/artwork-about-community.png"
+	"/assets/img/header-logo-agov.png",
+	"/assets/img/users-logo-dashboard.png",
+	"/assets/img/users-logo-dta.png",
+	"/assets/img/users-logo-dva.png",
+	"/assets/img/users-logo-health.png",
+	"/assets/img/users-logo-marketplace.png",
+	"/assets/img/users-logo-mygov.png"
 ];
 
 // Does the browser support service workers?
 if ("serviceWorker" in navigator) {
-
-  // Load and register this service worker configuration.
+	// Load and register this service worker configuration.
 	window.addEventListener("load", function() {
 		navigator.serviceWorker.register("/assets/js/serviceWorker.js").then(
 			function(registration) {
