753d03b35a99b09cd1352ec8944f7982311c7724
adamzerella <adamzerella@users.noreply.github.com>
2018-11-09 16:54:57 +1100

    Added missing description to layouts
    Corrected meta key name

--- src/layout/page.js
+++ src/layout/page.js
@@ -29,7 +29,6 @@ const Page = ({
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 <meta name="robots" content="index, follow">
-<meta name="description" content="Inclusive design, open-source code and shared insights">
 <meta name="author" content="Digital Transformation Agency">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="Australian Government Design System">
