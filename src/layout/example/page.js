c8ac1b998221c78e858b181becff0269385cc969
Raj <sukhrajghuman@live.com>
2018-11-13 14:04:39 +1100

    Update page.js

--- src/layout/example/page.js
+++ src/layout/example/page.js
@@ -47,7 +47,6 @@ const Example = ({
 <meta name="theme-color" content="#ffffff">
 <meta name="robots" content="index, follow">
 <meta name="author" content="Digital Transformation Agency">
-<meta name="description" content="Inclusive design, open-source code and shared insights">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="Australian Government Design System">
 <meta name="twitter:description" content="Inclusive design, open-source code and shared insights">
