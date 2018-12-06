22842de9ddc1063a2a18ffca1b2708d3f1ff82f2
Alex Page <alex@alexpage.com.au>
2018-12-06 15:15:02 +1100

    Templates landing page

--- content/templates/templates.md
+++ content/templates/templates.md
@@ -1,14 +1,9 @@
 ---
-layout: templates/template-card
+layout: templates/released
 templates:
-  - name: home
-    heading: Home page
-    body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum a suscipit corrupti. Corporis quo minus numquam necessitatibus aspernatur consequatur nihil totam at. Culpa harum ea sunt, deleniti voluptatum nisi quidem?
-  - name: basic
-    heading: Basic page
-    body: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore beatae magni fugit alias ullam impedit ea quam, quidem possimus earum ipsum numquam omnis pariatur distinctio? Quis eaque, dolor quos culpa, aliquam minima eligendi accusantium quibusdam quo qui alias quas voluptas asperiores explicabo ex? Nihil, officia similique! Quam harum ipsa sint.
+  - home
+  - basic
 cardList:
-  columnSize: col-xs-12
   appearance: shadow
 ---
-
+Choose from any of the template below to get your project up and running faster. They’ll save your team time and resources and help get value to your users sooner.
---
+++ src/layout/templates/released.js
@@ -0,0 +1,49 @@
+import PropTypes           from 'prop-types';
+import React, { Fragment } from 'react';
+import Fs                  from 'fs';
+import Path                from 'path';
+
+import AUbutton  from '../../_uikit/layout/buttons';
+
+/**
+ * The Discussion Intro component
+ */
+const TemplatesReleased = ({ templates, _body, _relativeURL, _parseYaml, _parseMD }) => {
+	const cards = templates.map( template => {
+		const templatesDir = Path.normalize( `${ __dirname }/../../../content/templates/` );
+		const templateYaml = ( `${ templatesDir }${ template }/index.yml` );
+		const templateData = _parseYaml( Fs.readFileSync( templateYaml, 'utf-8' ) );
+
+		const intro = _parseMD( Fs.readFileSync( `${ templatesDir }${ template }/intro.md`, 'utf-8' ) )
+
+		return (
+			<Fragment>
+				<a href={ template } className="template-card__image au-card au-card--shadow">
+					<img alt={ `${ template } page template` } src={ _relativeURL( `/assets/img/templates/${ template }.jpg` ) } />
+				</a>
+				<div className="template-card__content">
+					<h2 class="au-display-xl">{ templateData.pagetitle }</h2>
+					{ intro }
+					<ul className="au-btn__list au-btn__list--inline">
+						<li><AUbutton link={ template }>Documentation</AUbutton></li>
+						<li><AUbutton link={ `${ template }/customise` } as='secondary'>Customise</AUbutton></li>
+					</ul>
+				</div>
+			</Fragment>
+		);
+	});
+
+	return (
+		<Fragment>
+			{ _body }
+			<ul class="templates__released">
+				{ cards.map( ( card, i ) => (
+					<li key={ i } class="template-card au-card">{ card }</li>
+				)) }
+			</ul>
+		</Fragment>
+	)
+}
+
+
+export default TemplatesReleased;
