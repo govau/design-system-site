ec57cfc9178ba48bbb6f731d5bc7a740eaa78dbc
adamzerella <adamzerella@users.noreply.github.com>
2018-12-07 14:08:09 +1100

    Implemented missing functionality for customise page

--- src/assets/js/a11y-helpers.js
+++ src/assets/js/a11y-helpers.js
@@ -1,13 +1,27 @@
 var frames = document.querySelectorAll( 'iframe' )
 var a11yInputs = document.querySelectorAll( 'input[name="a11y"]' );
+var paletteInputs = document.querySelectorAll( 'input[name="palette"]' );
+
+var resetBtn = document.getElementsByName("btn-reset");
+
+// Reset button event handler
+resetBtn[0].addEventListener("click", function(){
+    ResetFilters( );
+});
 
 // Add event listners to each `a11y` input
 a11yInputs.forEach( item => {
     item.addEventListener("click", function(){
         AddA11yClass( item.id );
-
     });
 });
+
+paletteInputs.forEach( item => {
+    item.addEventListener("click", function(){
+        FillPaletteColors( item.id );
+    });
+});
+
 
 // Add class to all iframes
 function AddA11yClass( a11yProfile ){
@@ -15,3 +29,156 @@ function AddA11yClass( a11yProfile ){
 		item.setAttribute( 'class', `js-filter--${ a11yProfile }` )
 	})
 }
+
+function FillPaletteColors( elementId ) {
+	console.log(elementId);
+
+	var styles = {
+		mygov: {
+				text:           '',
+				action:         '#026540',
+				focus:          '#ac1523',
+				background:     '',
+				textDark:       '#ffffff',
+				actionDark:     '#f9f9f9',
+				focusDark:      '#D2E771',
+				backgroundDark: '#026540',
+		},
+		ato: {
+			text:           '',
+			action:         '#0D979B',
+			focus:          '',
+			background:     '',
+			textDark:       '#ffffff',
+			actionDark:     '#F2D814',
+			focusDark:      '#4569a0',
+			backgroundDark: '#002341',
+		}
+	}
+	
+		Object.keys( styles ).forEach( key => {			
+			if ( key === elementId ) {
+				FillFormColors( styles[ key ] )
+			}
+		})
+	
+}
+
+
+/**
+ * FillForm - Fills the form with the parameters in the URL
+ */
+function FillFormColors( paramObject = QueryToObject() ){
+	Object.entries( paramObject ).map( value => {
+		if ( value[0] ) {
+			var input = document.querySelector( `input[ id="${ value[ 0 ]}"]` );
+			input.value = value[ 1 ];
+		}
+	})
+}
+
+/**
+ * QueryToObject - Turns the url query into an object
+ */
+function QueryToObject() {
+	var paramObject = {};
+	
+	if ( window.location.search ) {
+		
+		var paramString = window.location.search
+		
+		paramString.substr( 1 ).split( '&' ).map( value => {
+			const keyValue = value.split( '=' );
+			paramObject[ keyValue[ 0 ] ] = decodeURIComponent( keyValue[ 1 ] ).split( '+' ).join( '' );
+		});
+	}
+	
+	return paramObject;
+}
+
+/**
+ * Prefill the form given a color palette selection ( e.g 'ato' prefills the form with ATO colors )
+ */
+function PrefillPalette( element ) {
+	var styles = {
+		mygov: {
+				text:           '',
+				action:         '#026540',
+				focus:          '#ac1523',
+				background:     '',
+				textDark:       '#ffffff',
+				actionDark:     '#f9f9f9',
+				focusDark:      '#D2E771',
+				backgroundDark: '#026540',
+		},
+		ato: {
+			text:           '',
+			action:         '#0D979B',
+			focus:          '',
+			background:     '',
+			textDark:       '#ffffff',
+			actionDark:     '#F2D814',
+			focusDark:      '#4569a0',
+			backgroundDark: '#002341',
+		}
+	}
+	
+	Object.keys( styles ).forEach( key => {
+		if ( key === element.id ) {
+			FillFormColors( styles[ key ] )
+		}
+	})
+}
+
+/**
+ * ApplyColors - Applies the colours to the iframe
+ */
+function ApplyColours( paramString = window.location.search ){
+	frames.forEach( item => {
+		item.src = `http://localhost:3000/chameleon${ paramString }`
+	})
+}
+
+/**
+ * ApplyA11yToFrames - Set a filter class to all frames given a selected a11y radio input selection.
+ * @param {*} filter - A11y filter selected ( e.g greyscale, deuteranopia )
+ */
+function ApplyA11yToFrames ( filterId  ) {
+	frames.forEach( item => {
+		item.setAttribute( 'class', `filter filter--${ filterId }` )
+	})
+}
+
+
+/**
+ * Function binding onchange a11y radio input
+ * @param {*} element 
+ */
+function ApplyFilter( element ) {
+	ApplyA11yToFrames( element.id );
+}
+
+
+/**
+ * Reset form input.
+ */
+function ResetFilters() {
+    // Reset form inputs
+    Object.entries( QueryToObject() ).map( value => {
+		if ( value[0] ) {
+			var input = document.querySelector( `input[ id="${ value[ 0 ]}"]` );
+			input.value = '';
+		}
+    })
+    
+    // Reset a11y inputs
+    a11yInputs.forEach( item => {
+		item.checked = false;
+    })
+}
+
+
+// On page load, apply the colours
+ApplyColours();
+
+FillFormColors();
--- src/layout/customise/page.js
+++ src/layout/customise/page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 /**
  * The example component
@@ -14,7 +14,7 @@ const Customise = ({
 	_parents,
 	_pages
 }) => {
-	const parentId = _parents[ _parents.length - 2 ];
+	const parentId = _parents[_parents.length - 2];
 	const parentData = _pages[parentId];
 
 	const headContent = `
@@ -22,13 +22,34 @@ const Customise = ({
 <meta name="viewport" content="width=device-width">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-<link rel="apple-touch-icon" sizes="180x180" href=${ _relativeURL('/assets/favicons/apple-touch-icon.png', _ID ) }>
-<link rel="icon" type="image/png" sizes="32x32" href=${ _relativeURL('/assets/favicons/favicon-32x32.png', _ID ) }>
-<link rel="icon" type="image/png" sizes="16x16" href=${ _relativeURL('/assets/favicons/favicon-16x16.png', _ID ) }>
-<link rel="manifest" href=${ _relativeURL('/assets/favicons/site.webmanifest', _ID ) }>
-<link rel="mask-icon" href=${ _relativeURL('/assets/favicons/safari-pinned-tab.svg', _ID ) } color="#06262d">
-<link rel="shortcut icon" href=${ _relativeURL('/assets/favicons/favicon.ico', _ID ) }>
-<meta name="msapplication-config" content=${ _relativeURL('/assets/favicons/browserconfig.xml', _ID ) }>
+<link rel="apple-touch-icon" sizes="180x180" href=${_relativeURL(
+		"/assets/favicons/apple-touch-icon.png",
+		_ID
+	)}>
+<link rel="icon" type="image/png" sizes="32x32" href=${_relativeURL(
+		"/assets/favicons/favicon-32x32.png",
+		_ID
+	)}>
+<link rel="icon" type="image/png" sizes="16x16" href=${_relativeURL(
+		"/assets/favicons/favicon-16x16.png",
+		_ID
+	)}>
+<link rel="manifest" href=${_relativeURL(
+		"/assets/favicons/site.webmanifest",
+		_ID
+	)}>
+<link rel="mask-icon" href=${_relativeURL(
+		"/assets/favicons/safari-pinned-tab.svg",
+		_ID
+	)} color="#06262d">
+<link rel="shortcut icon" href=${_relativeURL(
+		"/assets/favicons/favicon.ico",
+		_ID
+	)}>
+<meta name="msapplication-config" content=${_relativeURL(
+		"/assets/favicons/browserconfig.xml",
+		_ID
+	)}>
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 <meta name="robots" content="index, follow">
@@ -39,113 +60,184 @@ const Customise = ({
 <meta name="twitter:name" content="Australian Government Design System">
 <meta name="twitter:image" content="https://designsystem.gov.au/assets/favicons/designsystem.jpg">
 <meta property="og:type" content="website">
-<meta property="og:title" content="${ pagetitle } - Australian Government Design System">
+<meta property="og:title" content="${pagetitle} - Australian Government Design System">
 <meta property="og:site_name" content="Australian Government Design System">
 <meta property="og:description" content="Inclusive design, open-source code and shared insights">
 <meta property="og:image" content="https://designsystem.gov.au/assets/favicons/designsystem.jpg">
 <meta property="og:url" content="https://designsystem.gov.au">
 
-<title>${ pagetitle ? pagetitle : componentTitle + ` example` } - Australian Government Design System</title>
+<title>${
+		pagetitle ? pagetitle : componentTitle + ` example`
+	} - Australian Government Design System</title>
 
-<link rel="stylesheet" href=${ _relativeURL( '/assets/css/example.css', _ID ) } />
+<link rel="stylesheet" href=${_relativeURL("/assets/css/example.css", _ID)} />
 
 <!--[if lte IE 9]>
-	<script src="${ _relativeURL( '/assets/js/html5shiv.js', _ID ) }"></script>
-	<script src="${ _relativeURL( '/assets/js/respond.js', _ID ) }"></script>
+	<script src="${_relativeURL("/assets/js/html5shiv.js", _ID)}"></script>
+	<script src="${_relativeURL("/assets/js/respond.js", _ID)}"></script>
 <![endif]-->
 <script>var $html=document.documentElement;if($html.classList)$html.classList.remove("no-js"),$html.classList.add("js");else{var className="no-js";$html.className=$html.className.replace(new RegExp("(^|\\b)"+className.split(" ").join("|")+"(\\b|$)","gi")," "),$html.className+=" js"}</script>`;
 
 	return (
 		<html>
-		<head dangerouslySetInnerHTML={{ __html: headContent }}  />
-		<body className={ `au-grid live-demo"` }>
-			<header className="header__example">
-				<div className="container-fluid">
-					<div className="row">
-						<div className="col-md-12">
-							<a className="au-direction-link au-direction-link--left" href={ `/${ parentId }` }>
-								Go back to { parentData.pagetitle }
-							</a>
-						</div>
-					</div>
-				</div>
-			</header>
-			<svg
-				aria-hidden="true"
-				className="svg-filter"
-				xmlns="http://www.w3.org/2000/svg"
-				version="1.1">
-				<defs>
-					<filter id="deuteranopia">
-						<feColorMatrix
-							in="SourceGraphic"
-							type="matrix"
-							values="0.625, 0.375, 0,   0, 0
-									0.7,   0.3,   0,   0, 0
-									0,     0.3,   0.7, 0, 0
-									0,     0,     0,   1, 0"/>
-					</filter>
-					<filter id="tritanopia">
-						<feColorMatrix
-							in="SourceGraphic"
-							type="matrix"
-							values="0.95, 0.05,  0,     0, 0
-									0,    0.433, 0.567, 0, 0
-									0,    0.475, 0.525, 0, 0
-									0,    0,     0,     1, 0"/>
-					</filter>
-				</defs>
-			</svg>
-			<main>
-				<div className={`container-fluid content${ alignContent === 'center' ? ' content--center' : ''}`}>
-					<div className="row">
-						<div className="col-md-12 chameleon">
-							<h1>Customise</h1>
-							<iframe src={
-								process.env.NODE_ENV == "production" 
-								? "https://designsystem.gov.au/chameleon" 
-								: 'http://localhost:3000/chameleon'}
-							/>
-							<div className="accessibility-selection">
-								<h2>Accessibility Options</h2>
-								<label
-									className="lbl-grayscale"
-									>Greyscale
-									<input
-										name="a11y"
-										id="grayscale"
-										type="radio"
-									/>
-								</label>
-								<label
-									className="lbl-deuteranopia"
-									>Deuteranopia
-									<input
-										name="a11y"
-										id="deuteranopia"
-										type="radio"
-									/>
-								</label>
-								<label
-									className="lbl-tritanopia"
-									>Tritanopia
-									<input
-										name="a11y"
-										id="tritanopia"
-										type="radio"
-									/>
-								</label>
+			<head dangerouslySetInnerHTML={{ __html: headContent }} />
+			<body className={`au-grid live-demo"`}>
+				<header className="header__example">
+					<div className="container-fluid">
+						<div className="row">
+							<div className="col-md-12">
+								<a
+									className="au-direction-link au-direction-link--left"
+									href={`/${parentId}`}
+								>
+									Go back to {parentData.pagetitle}
+								</a>
 							</div>
 						</div>
 					</div>
-				</div>
-			</main>
-			{/* <script src={ _relativeURL( '/assets/js/iframe-resizer-contentWindow.js', _ID ) } /> */}
-			<script src={ _relativeURL( '/assets/js/footer.js', _ID ) } />
-			<script src={ _relativeURL( '/assets/js/a11y-helpers.js', _ID ) } />
-		</body>
+				</header>
+				<svg
+					aria-hidden="true"
+					className="svg-filter"
+					xmlns="http://www.w3.org/2000/svg"
+					version="1.1"
+				>
+					<defs>
+						<filter id="deuteranopia">
+							<feColorMatrix
+								in="SourceGraphic"
+								type="matrix"
+								values="0.625, 0.375, 0,   0, 0
+									0.7,   0.3,   0,   0, 0
+									0,     0.3,   0.7, 0, 0
+									0,     0,     0,   1, 0"
+							/>
+						</filter>
+						<filter id="tritanopia">
+							<feColorMatrix
+								in="SourceGraphic"
+								type="matrix"
+								values="0.95, 0.05,  0,     0, 0
+									0,    0.433, 0.567, 0, 0
+									0,    0.475, 0.525, 0, 0
+									0,    0,     0,     1, 0"
+							/>
+						</filter>
+					</defs>
+				</svg>
+				<main>
+					<div
+						className={`container-fluid content${
+							alignContent === "center" ? " content--center" : ""
+						}`}
+					>
+						<div className="row">
+							<div className="col-md-12 chameleon">
+								<h1>Customise</h1>
+								<iframe
+									src={
+										process.env.NODE_ENV == "production"
+											? "https://designsystem.gov.au/chameleon"
+											: "http://localhost:3000/chameleon"
+									}
+								/>
+								<h2>Input</h2>
+								<form id="au-form">
+									<div class="form-item">
+										<label>
+											Text
+											<input name="text" id="text" type="text" />
+										</label>
+									</div>
+									<div class="form-item">
+										<label>
+											Action
+											<input name="action" id="action" type="text" />
+										</label>
+									</div>
+									<div class="form-item">
+										<label>
+											Focus
+											<input name="focus" id="focus" type="text" />
+										</label>
+									</div>
+									<div class="form-item">
+										<label>
+											Background
+											<input name="background" id="background" type="text" />
+										</label>
+									</div>
+									<div class="form-item">
+										<label>
+											Dark text
+											<input name="textDark" id="textDark" type="text" />
+										</label>
+									</div>
+									<div class="form-item">
+										<label>
+											Dark action
+											<input name="actionDark" id="actionDark" type="text" />
+										</label>
+									</div>
+									<div class="form-item">
+										<label>
+											Dark focus
+											<input name="focusDark" id="focusDark" type="text" />
+										</label>
+									</div>
+									<div class="form-item">
+										<label>
+											Dark background
+											<input
+												name="backgroundDark"
+												id="backgroundDark"
+												type="text"
+											/>
+										</label>
+									</div>
+									<button>Change colours!</button>
+								</form>
+
+								<button name="btn-reset">Reset</button>
+
+								<div className="accessibility-selection">
+									<h2>Accessibility Options</h2>
+									<label className="lbl-grayscale">
+										Greyscale
+										<input name="a11y" id="grayscale" type="radio" />
+									</label>
+									<label className="lbl-deuteranopia">
+										Deuteranopia
+										<input name="a11y" id="deuteranopia" type="radio" />
+									</label>
+									<label className="lbl-tritanopia">
+										Tritanopia
+										<input name="a11y" id="tritanopia" type="radio" />
+									</label>
+								</div>
+
+								<div className="palette-selection">
+									<h2>Palette Options</h2>
+									<label className="lbl-palette-mygov">
+										myGov
+										<input name="palette" id="mygov" type="radio" />
+									</label>
+									<label className="lbl-palette-ato">
+										ATO
+										<input name="palette" id="ato" type="radio" />
+									</label>
+								</div>
+								<br />
+							</div>
+						</div>
+					</div>
+				</main>
+				{/* <script src={ _relativeURL( '/assets/js/iframe-resizer-contentWindow.js', _ID ) } /> */}
+				<script src={_relativeURL("/assets/js/footer.js", _ID)} />
+				<script src={_relativeURL("/assets/js/a11y-helpers.js", _ID)} />
+			</body>
 		</html>
-		)
+	);
 };
 
 Customise.propTypes = {};
