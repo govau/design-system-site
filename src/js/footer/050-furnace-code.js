86de62734121cc03a1b80b0b5d16b79219601b63
Adam Zerella <me@adamzerella.com>
2019-03-25 10:57:50 +1100

    Update src/js/footer/050-furnace-code.js
    Co-Authored-By: KiriHoy <34221033+KiriHoy@users.noreply.github.com>

--- src/js/footer/050-furnace-code.js
+++ src/js/footer/050-furnace-code.js
@@ -69,7 +69,7 @@ function ClearNPM() {
  * Select all button selects all checkbox elements on page
  */ 
 AddEvent( selectAll, 'click', function( event ){
-	furnaceComponents.forEach(function(furnaceComponents) {
+	furnaceComponents.forEach( function( furnaceComponents ) {
 		furnaceComponents.checked = true;
 	});
 });
