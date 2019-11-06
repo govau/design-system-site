82c6170b7d038c57dd4b336b8f718127582c9269
Anthony McKerron <47020580+anthonymckerron@users.noreply.github.com>
2019-11-05 16:07:47 -0800

    Feature/add contributors about (#709)
    * Adding dedicated about page
    
    * Adding home and about pages
    
    * Adding contributors section to the about page
    
    * Merging changes from develop.
    
    * Merge fix
    
    * Update src/layout/about/contributors.js
    
    Co-Authored-By: Gordon Grace <gordongrace@users.noreply.github.com>
    
    * Added spacing for contributors section for mobile

---
+++ src/layout/about/contributors-page.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react';
+import Contributors from './contributors';
+import Path from 'path';
+import Fs from 'fs';
+
+
+const ParseYaml = ( yaml ) => {
+
+	const YAML = require('js-yaml');
+
+	if( typeof yaml === 'string' ) {
+		try {
+			return YAML.safeLoad( yaml, warning => Log.error( warning ) ) || {};
+		}
+		catch( error ) {
+			console.error(`Rendering yaml caused an error`);
+			console.error( error );
+		}
+	}
+	else {
+		return yaml;
+	}
+}
+
+const ContributorsPage = ({ title, body, _relativeURL }) => {
+
+	const CONTRIBUTORS = ParseYaml( Fs.readFileSync( Path.normalize(`${ __dirname }/../../../content/about/_contributors.yml`), 'utf-8' ) );
+	var allContributors = [];
+
+	CONTRIBUTORS.allContributors.forEach(element => {
+		allContributors.push(element)
+	});
+
+	return (
+		<div className="row">
+			<div className="col-md-12">
+				<Fragment>
+					<h2>{ title }</h2>
+					<p>{ body }</p>
+					<p>
+						<Contributors
+							contributors={ allContributors }
+							_relativeURL={ _relativeURL }
+						/>
+					</p>
+				</Fragment>
+			</div>
+		</div>
+	)
+};
+
+export default ContributorsPage;
